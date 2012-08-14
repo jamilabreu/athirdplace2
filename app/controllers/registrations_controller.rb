31d4e8a2d64efc0da6e1b50a4ae987e918a27af2
Jamil Abreu <abreu.jamil@gmail.com>
2012-08-14 19:08:12 -0400

    commit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   skip_before_filter :validate_user, :only => [:edit, :update]
+
+  def edit
+    @alumni_id = Community.find_by(name: 'Alumni').id
+  end
 end
