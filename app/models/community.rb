c4eec3c252d3dd3e896e51c423bf34bcd769252c
Jamil Abreu <abreu.jamil@gmail.com>
2012-08-14 16:59:12 -0400

    commit

--- app/models/community.rb
+++ app/models/community.rb
@@ -3,13 +3,21 @@ class Community
   include Mongoid::Timestamps
   include Mongoid::Paranoia
   include Mongoid::Tree
+  include Geocoder::Model::Mongoid
+
+  has_and_belongs_to_many :users
   
   field :name, type: String
   field :subdomain, type: String
   field :display_name, type: String
   field :community_type, type: String
-  
-  has_and_belongs_to_many :users
+  field :state, type: String
+  field :state_code, type: String
+  field :country, type: String
+  field :country_code, type: String
+  field :coordinates, type: Array
+  field :name_singular, type: String
+  field :name_plural, type: String
   
   scope :filtered_by, lambda { |community_type| where(community_type: community_type.to_s.titleize).asc(:name) }
 
