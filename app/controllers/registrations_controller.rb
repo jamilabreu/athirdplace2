822b0d40cc833f5a1316b1d92c01e3ce53937fbb
Jamil Abreu <abreu.jamil@gmail.com>
2012-08-19 20:40:41 -0400

    commit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
   skip_before_filter :validate_user, :only => [:edit, :update]
+  protected
+
+    def after_update_path_for(resource)
+      root_path
+    end
 end
