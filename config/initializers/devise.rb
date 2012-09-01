4f0432cb5c5206d91e43bdd75fd8c9525b33b268
Jamil Abreu <abreu.jamil@gmail.com>
2012-09-01 03:44:01 -0400

    commit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -205,6 +205,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], :scope => 'email'
+  #config.omniauth :linkedin, 'tuzbf0oh3kpk', 'ed9tSJwx1SNDMNA0'
   
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
